ffc8b7d00ccb286fd969662eb203028e8cbd10b4
Steve <zjplove3618739792@gmail.com>
2024-06-03 15:53:45 -0400

    message describing code changes

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override"); 
+const morgan = require("morgan");
 
 
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URI);
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); 
+app.use(morgan("dev")); 
 
+
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
 const House = require('./models/house.js');
 
 app.get("/", async (req, res) => {
@@ -30,7 +35,7 @@ app.get("/houses", async (req, res) => {
 app.get("/houses/detail", async (req, res) => {
   res.render("houses/new.ejs");
 });
-app.use(express.urlencoded({ extended: false }));
+
 
 app.post("/houses", async (req, res) => {
   if (req.body.isReadyToBook === "on") {
@@ -47,6 +52,32 @@ app.get("/houses/:houseId", async (req, res) => {
   res.render("houses/show.ejs", { house: foundHouse });
 });
 
+app.delete("/houses/:houseId", async (req, res) => {
+  await House.findByIdAndDelete(req.params.houseId);
+  res.redirect("/houses");
+});
+
+app.get("/houses/:houseId/edit", async (req, res) => {
+  const foundHouse = await House.findById(req.params.houseId);
+  res.render("houses/edit.ejs", {
+    house: foundHouse,
+  });
+});
+
+
+
+app.put("/houses/:houseId", async (req, res) => {
+
+  if (req.body.isReadyToBook === "on") {
+    req.body.isReadyToBook = true;
+  } else {
+    req.body.isReadyToBook = false;
+  }
+  
+  await House.findByIdAndUpdate(req.params.houseId, req.body);
+
+  res.redirect(`/houses/${req.params.houseId}`);
+});
 
 
 app.listen(4000, () => {
